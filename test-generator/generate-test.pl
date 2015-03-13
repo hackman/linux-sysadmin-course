6579873902c9ebb47726efe110718dea37aff610
Marian Marinov <mm@yuhu.biz>
2015-03-13 04:08:55 +0200

    test-generator/generate-test.pl: - Added dynamic generation of test name - Fixed a stupid select count(*) query - Added check if we have issues with queies to PgSQL(like permission denied :)) - Added missing execution of $first_qid, which breaks the generation of tests for test 2 - Removed the $qcount limit in $get_question - Removed the stupid shadow over the answers - Centered all correct answers
    Signed-off-by: Marian Marinov <mm@yuhu.biz>

--- test-generator/generate-test.pl
+++ test-generator/generate-test.pl
@@ -23,7 +23,7 @@ my $pgdb = 'DBI:Pg:database=smal;host=localhost;port=5432';
 my $pgconn = DBI->connect_cached( $pgdb, $pguser, $pgpass, { PrintError => 1, AutoCommit => 1 }) or die("$DBI::errstr\n");
 my $schema = 'public';
 # define which test we are going to generate, first or second
-my $first_test = 1;
+my $first_test = 0;
 my %ques = ();
 my $qcount = 0;
 my $question_count = 1;
@@ -32,20 +32,34 @@ my $variant = 1;
 $variant = $ARGV[0] if (defined($ARGV[0]) && $ARGV[0] =~ /^[0-9]+$/);
 my $filename = "test1-variant$variant";
 my $first_qid;
+my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
 
-print "Generating test variant $variant\n";
+$year = $year+1900;
+
+my $test = $first_test ? 1 : 2;
+print "Generating TEST $test $year variant $variant\n";
 
 $pgconn->{pg_enable_utf8}=1;
 # get the total count of questions in the table
-my $get_qcount = $pgconn->prepare(sprintf('SELECT count(*) FROM "%s".questions WHERE %s first_test', $schema, $first_test ? '' : 'NOT' ))
+my $get_qcount = $pgconn->prepare(sprintf('SELECT count(id) FROM "%s".questions WHERE %s first_test', $schema, $first_test ? '' : 'NOT' ))
 	or logger($DBI::errstr);
-$get_qcount->execute() or logger($DBI::errstr);
+if (!$get_qcount->execute()) {
+	logger($DBI::errstr);
+	exit 1;
+}
 $qcount = $get_qcount->fetchrow_array or logger($DBI::errstr);
 if ($first_test) {
+	$first_qid = $pgconn->prepare(sprintf('SELECT id FROM "%s".questions WHERE first_test ORDER BY id LIMIT 1', $schema ))
+		or logger($DBI::errstr);
+} else {
 	$first_qid = $pgconn->prepare(sprintf('SELECT id FROM "%s".questions WHERE NOT first_test ORDER BY id LIMIT 1', $schema ))
+		or logger($DBI::errstr);
+}
+my $fqid = $first_qid->execute();
+print "First quid: $fqid\n" if $debug;
 
 #my $get_question   = $pgconn->prepare(sprintf('SELECT question FROM "%s".questions WHERE id = ? OFFSET random()*%d LIMIT 1', $schema, $qcount))
-my $get_question   = $pgconn->prepare(sprintf('SELECT question FROM "%s".questions WHERE id = ? LIMIT 1', $schema, $qcount))
+my $get_question   = $pgconn->prepare(sprintf('SELECT question FROM "%s".questions WHERE id = ? LIMIT 1', $schema))
 	or logger($DBI::errstr);
 my $right_asnwares = $pgconn->prepare(sprintf('SELECT answer FROM "%s".right_answers WHERE q_id = ?', $schema))
 	or logger($DBI::errstr);
@@ -62,7 +76,7 @@ my %questions_check = ();
 my %excluded = ();
 
 my $id_compensation = 1;
-$id_compensation = $first_qid-1 if !$first_test;
+$id_compensation = $fqid-1 if !$first_test;
 my $id = $id_compensation + int(rand($qcount));
 # generate random question positions
 while ($question_count <= $max_questions) {
@@ -118,7 +132,7 @@ sub page_check {
 prFile("$filename.pdf");
 prTTFont('/usr/share/fonts/arial.ttf');
 prText(35,800,"Linux System & Network Administration");
-prText(340,800,"TEST 1   2015");
+prText(340,800,"TEST $test   $year");
 prText(532,800,"Page $page_count");
 prText(35,780,"Name: ______________________________________________________________________");
 prText(35,760,"SoftUni username: ______________________________");
@@ -250,26 +264,27 @@ for (1..$max_questions) {
 	$right = 'b' if ($ques{$_}[0] == 2);
 	$right = 'c' if ($ques{$_}[0] == 3);
 	$right = 'd' if ($ques{$_}[0] == 4);
- 	printf("%2d: %s \n",$tcount, $right) if $debug;
- 	prAdd("q 40 717 m 490 717 l S Q");
- 	prAdd("q 40 698 m 490 698 l S Q");
- 	prAdd("q 40 677 m 490 677 l S Q");
- 	prAdd("q 40 658 m 490 658 l S Q");
- 	prAdd("q 490 698 m 490 717 l S Q");
- 	prAdd("q 490 658 m 490 677 l S Q");
+	printf("%2d: %s \n",$tcount, $right) if $debug;
+	prAdd("q 38 717 m 490 717 l S Q");
+	prAdd("q 38 698 m 490 698 l S Q");
+	prAdd("q 38 677 m 490 677 l S Q");
+	prAdd("q 38 658 m 490 658 l S Q");
+	prAdd("q 490 698 m 490 717 l S Q");
+	prAdd("q 490 658 m 490 677 l S Q");
  	if ($tcount <= 25) {
 		my $pos = ($tcount * 18) + 24;
-		my $pos2 =  $pos - 2;
+		my $pos2 =  $pos - 4;
 		prAdd("q $pos2 698 m $pos2 717 l S Q");
+
 # 		prAdd("9.0 9.0 9.0 rg\n");
 #  		prAdd("$pos2 700 18 20 re\n");
 #  		prAdd("t\n");
-		prText($pos,702,$right);
+		prText($pos,704,$right);
 	} else {
 		my $pos = (($tcount - 25) * 18) + 24;
-		my $pos2 = $pos - 2;
+		my $pos2 = $pos - 4;
 		prAdd("q $pos2 658 m $pos2 677 l S Q");
-		prText($pos,662,$right);
+		prText($pos,663,$right);
 	}
 
 	$tcount++;
@@ -277,8 +292,8 @@ for (1..$max_questions) {
 
 for my $l (1..25) {
  	my $pos = ($l * 18) + 20;
- 	prAdd("$pos 700 18 20 re\n");
- 	prAdd("$pos 660 18 20 re\n");
+# 	prAdd("$pos 698 18 20 re\n");
+# 	prAdd("$pos 660 18 20 re\n");
  }
  prAdd("B\n");
 prEnd();
