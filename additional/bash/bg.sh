f81baf1d1aaf3a31d6e9cd14d5c380af89106a9d
Marian Marinov <mm@yuhu.biz>
2021-09-05 07:03:35 +0300

    additional/bash/bg.sh: A more advanced and efficient version of the background implementation
    The top version shows how to wait for all processes that you have started to finish, before new processes get spawned.
    
    This is very inefficient when you wait for example for long running disk operations. So the new iplementation solves
    that by starting a new process, the moment another one from the list dies.
    
    Signed-off-by: Marian Marinov <mm@yuhu.biz>

--- additional/bash/bg.sh
+++ additional/bash/bg.sh
@@ -24,3 +24,34 @@ for i in {1..5}; do
 		let total--
 	done
 done
+
+# A more optimal solution then the above one
+
+count=0
+# tasks will be used to store information about the pid
+declare -A tasks
+for i in {1..5}; do
+	procs=()
+	var=$((${RANDOM}%10+1))
+	sleep $var &
+	cmd_pid=$!
+	procs+=($cmd_pid)
+	tasks[$cmd_pid]="$i $var"
+
+    let count++
+    if [[ $count -eq $max_threads ]]; then
+        echo "Waiting for ${procs[*]}"
+        # Wait for any task to finish.
+        # After a task has finished, remove it from the array and reduce the count of currently running tasks.
+        # This will allow us to start the next process immediately.
+        pid=''
+        wait -p pid -n ${procs}
+        echo "Task PID: $pid finished"
+        echo ${tasks[$pid]} >> $finished_tasks
+		all_procs=${procs[*]}
+        procs=( $(echo ${all_procs//$pid}) )
+
+        let count--
+    fi
+
+done
